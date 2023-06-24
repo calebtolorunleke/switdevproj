40945bafb2fbb8db622eb4579b407eff46c0d7f8
Caleb Adebayo <72946335+calebtolorunleke@users.noreply.github.com>
2023-06-24 08:43:35 +0000

    Owolabi

--- src/App.js
+++ src/App.js
@@ -16,4 +16,32 @@ function App() {
   return <div className="App"></div>;
 }
 
+
+const App = () => {
+  const style = {
+    fontFamily: "Poppins, sans-serif",
+    fontSize: "16px",
+    fontWeight: "400",
+    backgroundColor: "#EBF5F6",
+  };
+
+  return (
+    <div style={style}>
+      <BrowserRouter>
+        <List />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="Services" element={<Fail />} />
+          <Route path="Success" element={<Success />} />
+          <Route path="Login" element={<Login />} />
+          <Route path="ccess" element={<Logincess />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
+
+
 export default App;
