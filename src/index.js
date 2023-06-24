420311cbef6882f9000203f01dd259464d97419f
Caleb Adebayo <72946335+calebtolorunleke@users.noreply.github.com>
2023-06-24 10:09:11 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,15 +9,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
-import React from "react";
-import "./index.css";
-import App from "./App";
-//import reportWebVitals from "./reportWebVitals";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css";
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+
 root.render(
   <StrictMode>
     <App />
